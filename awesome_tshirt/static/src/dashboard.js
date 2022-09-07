26109aaf7f7e89509363659d9517531675e08f79
Dardenne Florent (dafl) <dafl@odoo.com>
2022-09-07 11:36:41 +0200

    [Solution] 1.2 A Todo Component

--- awesome_tshirt/static/src/dashboard.js
+++ awesome_tshirt/static/src/dashboard.js
@@ -1,13 +1,19 @@
 /** @odoo-module **/
 
+
 import { Counter } from "./counter/counter";
+import { Todo } from "./todo/todo";
 import { registry } from "@web/core/registry";
 
 const { Component } = owl;
 
-class AwesomeDashboard extends Component {}
+class AwesomeDashboard extends Component {
+    setup() {
+        this.todo = { id: 3, description: "buy milk", done: false };
+    }
+}
 
-AwesomeDashboard.components = { Counter };
+AwesomeDashboard.components = { Counter, Todo };
 AwesomeDashboard.template = "awesome_tshirt.clientaction";
 
 registry.category("actions").add("awesome_tshirt.dashboard", AwesomeDashboard);
---
+++ awesome_tshirt/static/src/todo/todo.js
@@ -0,0 +1,7 @@
+/** @odoo-module */
+
+const { Component } = owl;
+
+export class Todo extends Component {}
+
+Todo.template = "awesome_tshirt.Todo";
