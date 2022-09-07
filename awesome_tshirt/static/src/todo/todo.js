8012da43b5144873bade091b71fcbb5dfb535a57
Dardenne Florent (dafl) <dafl@odoo.com>
2022-09-07 13:17:35 +0200

    [Solution] 1.3. Props Validation

--- awesome_tshirt/static/src/todo/todo.js
+++ awesome_tshirt/static/src/todo/todo.js
@@ -5,3 +5,8 @@ const { Component } = owl;
 export class Todo extends Component {}
 
 Todo.template = "awesome_tshirt.Todo";
+Todo.props = {
+    id: { type: Number },
+    description: { type: String },
+    done: { type: Boolean },
+};
