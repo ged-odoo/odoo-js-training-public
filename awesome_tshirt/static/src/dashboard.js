94c82ed6f8550b2cd7f6db668e2c171fa4aac93a
Dardenne Florent (dafl) <dafl@odoo.com>
2022-09-08 08:59:28 +0200

    [Solution] 2.0 Reset the client_action

--- awesome_tshirt/static/src/dashboard.js
+++ awesome_tshirt/static/src/dashboard.js
@@ -1,15 +1,12 @@
 /** @odoo-module **/
 
-import { Counter } from "./counter/counter";
-import { TodoList } from "./todo_list/todo_list";
-import { Card } from "./card/card";
 import { registry } from "@web/core/registry";
 
 const { Component } = owl;
 
 class AwesomeDashboard extends Component {}
 
-AwesomeDashboard.components = { Counter, TodoList, Card };
+AwesomeDashboard.components = {};
 AwesomeDashboard.template = "awesome_tshirt.clientaction";
 
 registry.category("actions").add("awesome_tshirt.dashboard", AwesomeDashboard);
