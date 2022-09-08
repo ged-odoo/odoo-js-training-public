a24c5710d3a7db43629ff8f8be560706e648c36e
Dardenne Florent (dafl) <dafl@odoo.com>
2022-09-08 10:56:37 +0200

    [Solution] 2.1. A new Layout

--- awesome_tshirt/static/src/dashboard.js
+++ awesome_tshirt/static/src/dashboard.js
@@ -1,12 +1,26 @@
 /** @odoo-module **/
 
 import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout";
+import { getDefaultConfig } from "@web/views/view";
 
-const { Component } = owl;
+const { Component, useSubEnv } = owl;
 
-class AwesomeDashboard extends Component {}
+class AwesomeDashboard extends Component {
+    setup() {
+        useSubEnv({
+            config: {
+                ...getDefaultConfig(),
+                ...this.env.config,
+            },
+        });
+        this.display = {
+            controlPanel: { "top-right": false, "bottom-right": false },
+        };
+    }
+}
 
-AwesomeDashboard.components = {};
+AwesomeDashboard.components = { Layout };
 AwesomeDashboard.template = "awesome_tshirt.clientaction";
 
 registry.category("actions").add("awesome_tshirt.dashboard", AwesomeDashboard);
