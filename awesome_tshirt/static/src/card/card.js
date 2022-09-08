6adecde5a265517b20d084433096cb2b70a59409
Dardenne Florent (dafl) <dafl@odoo.com>
2022-09-08 12:58:23 +0200

    [Solution] 1.10 Miscellaneous small tasks

--- awesome_tshirt/static/src/card/card.js
+++ awesome_tshirt/static/src/card/card.js
@@ -5,3 +5,12 @@ const { Component } = owl;
 export class Card extends Component {}
 
 Card.template = "awesome_tshirt.Card";
+Card.props = {
+    slots: {
+        type: Object,
+        shape: {
+            default: Object,
+            title: { type: Object, optional: true },
+        },
+    },
+};
