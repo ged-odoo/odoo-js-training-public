de92f3dc330948502a12b3a52e102c36fbdc7852
Dardenne Florent (dafl) <dafl@odoo.com>
2022-09-08 15:11:45 +0200

    [Solution] 2.2 Add some buttons for quick navigation

--- awesome_tshirt/static/src/dashboard.js
+++ awesome_tshirt/static/src/dashboard.js
@@ -3,6 +3,8 @@
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
 import { getDefaultConfig } from "@web/views/view";
+import { useService } from "@web/core/utils/hooks";
+import { Domain } from "@web/core/domain";
 
 const { Component, useSubEnv } = owl;
 
@@ -17,6 +19,36 @@ class AwesomeDashboard extends Component {
         this.display = {
             controlPanel: { "top-right": false, "bottom-right": false },
         };
+
+        this.action = useService("action");
+    }
+
+    openCustomerView() {
+        this.action.doAction("base.action_partner_form");
+    }
+
+    openOrders(title, domain) {
+        this.action.doAction({
+            type: "ir.actions.act_window",
+            name: title,
+            res_model: "awesome_tshirt.order",
+            domain: new Domain(domain).toList(),
+            views: [
+                [false, "list"],
+                [false, "form"],
+            ],
+        });
+    }
+    openLast7DaysOrders() {
+        const domain =
+            "[('create_date','>=', (context_today() - datetime.timedelta(days=7)).strftime('%Y-%m-%d'))]";
+        this.openOrders("Last 7 days orders", domain);
+    }
+
+    openLast7DaysCancelledOrders() {
+        const domain =
+            "[('create_date','>=', (context_today() - datetime.timedelta(days=7)).strftime('%Y-%m-%d')), ('state','=', 'cancelled')]";
+        this.openOrders("Last 7 days cancelled orders", domain);
     }
 }
 
