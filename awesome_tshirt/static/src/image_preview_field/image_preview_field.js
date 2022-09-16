8e36404373796a84846769ae711224334b947ba4
Dardenne Florent (dafl) <dafl@odoo.com>
2022-09-16 12:47:10 +0200

    [Solution] 4.4 Add a command to the command palette

--- awesome_tshirt/static/src/image_preview_field/image_preview_field.js
+++ awesome_tshirt/static/src/image_preview_field/image_preview_field.js
@@ -2,10 +2,16 @@
 
 import { registry } from "@web/core/registry";
 import { CharField } from "@web/views/fields/char/char_field";
-
+import { useCommand } from "@web/core/commands/command_hook";
 const { Component } = owl;
 
-class ImagePreviewField extends Component {}
+class ImagePreviewField extends Component {
+    setup() {
+        useCommand(this.env._t("Open image in a new tab"), () => {
+            window.open(this.props.value, "_blank");
+        });
+    }
+}
 
 ImagePreviewField.template = "awesome_tshirt.ImagePreviewField";
 ImagePreviewField.components = { CharField };
