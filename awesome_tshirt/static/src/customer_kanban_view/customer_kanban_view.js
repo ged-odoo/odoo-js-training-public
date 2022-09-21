5801e22d12d4612954551a9b6128bf910e064d5f
Dardenne Florent (dafl) <dafl@odoo.com>
2022-09-21 11:41:08 +0200

    [Solution] 5.2 Create a CustomerList component

--- awesome_tshirt/static/src/customer_kanban_view/customer_kanban_view.js
+++ awesome_tshirt/static/src/customer_kanban_view/customer_kanban_view.js
@@ -2,9 +2,22 @@
 import { KanbanController } from "@web/views/kanban/kanban_controller";
 import { kanbanView } from "@web/views/kanban/kanban_view";
 import { registry } from "@web/core/registry";
+import { CustomerList } from "../customer_list/customer_list";
 
-class CustomerKanbanController extends KanbanController {}
+class CustomerKanbanController extends KanbanController {
+    setup() {
+        super.setup();
+        this.archInfo = { ...this.props.archInfo };
+        this.archInfo.className += " flex-grow-1";
+    }
 
+    selectCustomer(customer_id) {
+        console.log(customer_id);
+    }
+}
+
+CustomerKanbanController.components = { ...KanbanController.components, CustomerList };
+CustomerKanbanController.template = "awesome_tshirt.CustomerKanbanView";
 export const customerKanbanView = {
     ...kanbanView,
     Controller: CustomerKanbanController,
