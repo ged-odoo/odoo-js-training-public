6e20138432248b1d91e94b76761d681d3e0abd99
Dardenne Florent (dafl) <dafl@odoo.com>
2022-09-23 15:32:17 +0200

    [Solution] 5.5 Only display customers which have an active order

--- awesome_tshirt/static/src/customer_list/customer_list.js
+++ awesome_tshirt/static/src/customer_list/customer_list.js
@@ -1,15 +1,29 @@
 /** @odoo-module */
 
 import { useService } from "@web/core/utils/hooks";
+import { KeepLast } from "@web/core/utils/concurrency";
 
-const { Component, onWillStart } = owl;
+const { Component, onWillStart, useState } = owl;
 
 export class CustomerList extends Component {
     setup() {
         this.orm = useService("orm");
+        this.partners = useState({ data: [] });
+        this.domain = [];
+        this.keepLast = new KeepLast();
         onWillStart(async () => {
-            this.partners = await this.orm.searchRead("res.partner", [], ["display_name"]);
+            this.partners.data = await this.loadCustomers();
         });
+    }
+
+    async onChangeActiveCustomers(ev) {
+        const checked = ev.target.checked;
+        this.domain = checked ? [["has_active_order", "=", true]] : [];
+        this.partners.data = await this.keepLast.add(this.loadCustomers());
+    }
+
+    loadCustomers() {
+        return this.orm.searchRead("res.partner", this.domain, ["display_name"]);
     }
 }
 
