5602c0ccc6f482fb6f87f457ca5b2ba26e2ca11d
Dardenne Florent (dafl) <dafl@odoo.com>
2022-09-23 15:42:32 +0200

    [Solution] 5.6 Add a search bar to Customer List

--- awesome_tshirt/static/src/customer_list/customer_list.js
+++ awesome_tshirt/static/src/customer_list/customer_list.js
@@ -2,6 +2,7 @@
 
 import { useService } from "@web/core/utils/hooks";
 import { KeepLast } from "@web/core/utils/concurrency";
+import { fuzzyLookup } from "@web/core/utils/search";
 
 const { Component, onWillStart, useState } = owl;
 
@@ -9,10 +10,13 @@ export class CustomerList extends Component {
     setup() {
         this.orm = useService("orm");
         this.partners = useState({ data: [] });
+        this.displayedPartners = useState({ data: [] });
         this.domain = [];
         this.keepLast = new KeepLast();
+        this.filterName = "";
         onWillStart(async () => {
             this.partners.data = await this.loadCustomers();
+            this.displayedPartners.data = this.partners.data;
         });
     }
 
@@ -20,10 +24,28 @@ export class CustomerList extends Component {
         const checked = ev.target.checked;
         this.domain = checked ? [["has_active_order", "=", true]] : [];
         this.partners.data = await this.keepLast.add(this.loadCustomers());
+        this.filterCustomers(this.filterName);
     }
 
     loadCustomers() {
         return this.orm.searchRead("res.partner", this.domain, ["display_name"]);
+    }
+
+    onCustomerFilter(ev) {
+        this.filterName = ev.target.value;
+        this.filterCustomers(ev.target.value);
+    }
+
+    filterCustomers(name) {
+        if (name) {
+            this.displayedPartners.data = fuzzyLookup(
+                name,
+                this.partners.data,
+                (partner) => partner.display_name
+            );
+        } else {
+            this.displayedPartners.data = this.partners.data;
+        }
     }
 }
 
