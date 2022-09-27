5d848aea097204002842a64b2d9edd385b981ed1
Dardenne Florent (dafl) <dafl@odoo.com>
2022-09-27 15:53:01 +0200

    [Solution] 6.2  Use the Layout component

--- awesome_gallery/static/src/gallery_controller.js
+++ awesome_gallery/static/src/gallery_controller.js
@@ -1,7 +1,10 @@
 /** @odoo-module */
 
+import { Layout } from "@web/search/layout";
+
 const { Component } = owl;
 
 export class GalleryController extends Component {}
 
 GalleryController.template = "awesome_gallery.View";
+GalleryController.components = { Layout };
