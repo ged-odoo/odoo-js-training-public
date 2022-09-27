daddde04d6a280725aa4a9ee37626aa9de278327
Dardenne Florent (dafl) <dafl@odoo.com>
2022-09-27 18:19:23 +0200

    [Solution] 6.4 Load some data

--- awesome_gallery/static/src/gallery_controller.js
+++ awesome_gallery/static/src/gallery_controller.js
@@ -1,10 +1,36 @@
 /** @odoo-module */
 
 import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
+import { KeepLast } from "@web/core/utils/concurrency";
+const { Component, onWillStart, onWillUpdateProps, useState } = owl;
 
-const { Component } = owl;
+export class GalleryController extends Component {
+    setup() {
+        this.orm = useService("orm");
+        this.images = useState({ data: [] });
+        this.keeplast = new KeepLast();
+        onWillStart(async () => {
+            const { records } = await this.loadImages(this.props.domain);
+            this.images.data = records;
+        });
 
-export class GalleryController extends Component {}
+        onWillUpdateProps(async (nextProps) => {
+            if (JSON.stringify(nextProps.domain) !== JSON.stringify(this.props.domain)) {
+                const { records } = await this.loadImages(nextProps.domain);
+                this.images.data = records;
+            }
+        });
+    }
+
+    loadImages(domain) {
+        return this.keeplast.add(
+            this.orm.webSearchRead(this.props.resModel, domain, [this.props.archInfo.imageField], {
+                limit: this.props.archInfo.limit,
+            })
+        );
+    }
+}
 
 GalleryController.template = "awesome_gallery.View";
 GalleryController.components = { Layout };
