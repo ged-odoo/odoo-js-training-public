9de9f2b51c6af4c28322593b45e5ced15534a40b
Dardenne Florent (dafl) <dafl@odoo.com>
2022-09-28 15:45:14 +0200

    [Solution] 6.6 Display images

---
+++ awesome_gallery/static/src/gallery_image/gallery_image.js
@@ -0,0 +1,12 @@
+/** @odoo-module */
+
+const { Component } = owl;
+
+export class GalleryImage extends Component {}
+
+GalleryImage.template = "awesome_gallery.GalleryImage";
+GalleryImage.props = {
+    image: { type: Object },
+    className: { type: String },
+    imageField: { type: String },
+};
--- awesome_gallery/static/src/gallery_renderer.js
+++ awesome_gallery/static/src/gallery_renderer.js
@@ -1,7 +1,9 @@
 /** @odoo-module */
 
 const { Component } = owl;
+import { GalleryImage } from "./gallery_image/gallery_image";
 
 export class GalleryRenderer extends Component {}
 
+GalleryRenderer.components = { GalleryImage };
 GalleryRenderer.template = "awesome_gallery.Renderer";
