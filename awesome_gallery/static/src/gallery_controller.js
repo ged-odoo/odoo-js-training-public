19501faadb9ff15394c07023afb09a0f02000787
Dardenne Florent (dafl) <dafl@odoo.com>
2022-09-28 10:16:53 +0200

    [Solution] 6.5 Reorganize code

--- awesome_gallery/static/src/gallery_controller.js
+++ awesome_gallery/static/src/gallery_controller.js
@@ -2,33 +2,31 @@
 
 import { Layout } from "@web/search/layout";
 import { useService } from "@web/core/utils/hooks";
-import { KeepLast } from "@web/core/utils/concurrency";
+
 const { Component, onWillStart, onWillUpdateProps, useState } = owl;
 
 export class GalleryController extends Component {
     setup() {
         this.orm = useService("orm");
-        this.images = useState({ data: [] });
-        this.keeplast = new KeepLast();
+
+        this.model = useState(
+            new this.props.Model(
+                this.orm,
+                this.props.resModel,
+                this.props.archInfo,
+                this.props.domain
+            )
+        );
         onWillStart(async () => {
-            const { records } = await this.loadImages(this.props.domain);
-            this.images.data = records;
+            await this.model.load();
         });
 
         onWillUpdateProps(async (nextProps) => {
             if (JSON.stringify(nextProps.domain) !== JSON.stringify(this.props.domain)) {
-                const { records } = await this.loadImages(nextProps.domain);
-                this.images.data = records;
+                this.model.domain = nextProps.domain;
+                await this.model.load();
             }
         });
-    }
-
-    loadImages(domain) {
-        return this.keeplast.add(
-            this.orm.webSearchRead(this.props.resModel, domain, [this.props.archInfo.imageField], {
-                limit: this.props.archInfo.limit,
-            })
-        );
     }
 }
 
