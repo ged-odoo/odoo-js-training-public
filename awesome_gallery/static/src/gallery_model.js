4e1735f1488b99d9cfae7ec611802bf0f511c3ad
Dardenne Florent (dafl) <dafl@odoo.com>
2022-09-29 10:27:36 +0200

    [Solution] 6.8 Add an optional tooltip

--- awesome_gallery/static/src/gallery_model.js
+++ awesome_gallery/static/src/gallery_model.js
@@ -3,22 +3,45 @@
 import { KeepLast } from "@web/core/utils/concurrency";
 
 export class GalleryModel {
-    constructor(orm, resModel, archInfo, domain) {
+    constructor(orm, resModel, fields, archInfo, domain) {
         this.orm = orm;
         this.resModel = resModel;
-        const { imageField, limit } = archInfo;
+        const { imageField, limit, tooltipField } = archInfo;
         this.imageField = imageField;
+        this.fields = fields;
         this.limit = limit;
         this.domain = domain;
+        this.tooltipField = tooltipField;
         this.keepLast = new KeepLast();
     }
 
     async load() {
         const { records } = await this.keepLast.add(
-            this.orm.webSearchRead(this.resModel, this.domain, [this.imageField], {
-                limit: this.limit,
-            })
+            this.orm.webSearchRead(
+                this.resModel,
+                this.domain,
+                [this.imageField, this.tooltipField],
+                {
+                    limit: this.limit,
+                }
+            )
         );
-        this.images = records;
+
+        switch (this.fields[this.tooltipField].type) {
+            case "many2one":
+                this.images = records.map((record) => ({
+                    ...record,
+                    [this.tooltipField]: record[this.tooltipField][1],
+                }));
+                break;
+            case "integer":
+                this.images = records.map((record) => ({
+                    ...record,
+                    [this.tooltipField]: String(record[this.tooltipField]),
+                }));
+                break;
+            default:
+                this.images = records;
+        }
     }
 }
