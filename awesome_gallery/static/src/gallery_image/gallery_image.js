bed60e0e2b8d531cadc8030d608c45e4f6e1fb91
Dardenne Florent (dafl) <dafl@odoo.com>
2022-09-29 08:13:25 +0200

    [Solution] 6.7 Switch to form view on click

--- awesome_gallery/static/src/gallery_image/gallery_image.js
+++ awesome_gallery/static/src/gallery_image/gallery_image.js
@@ -2,11 +2,16 @@
 
 const { Component } = owl;
 
-export class GalleryImage extends Component {}
+export class GalleryImage extends Component {
+    onClick() {
+        this.props.onClick(this.props.image.id);
+    }
+}
 
 GalleryImage.template = "awesome_gallery.GalleryImage";
 GalleryImage.props = {
     image: { type: Object },
     className: { type: String },
     imageField: { type: String },
+    onClick: { type: Function },
 };
