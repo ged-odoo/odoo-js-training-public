4371d7c6e5254214b032d7fbe4bcf207ccfa6dd3
Dardenne Florent (dafl) <dafl@odoo.com>
2022-09-29 15:42:57 +0200

    [Solution] 5.8 Paginate customers!

--- awesome_tshirt/static/src/customer_list/customer_list.js
+++ awesome_tshirt/static/src/customer_list/customer_list.js
@@ -3,6 +3,7 @@
 import { useService } from "@web/core/utils/hooks";
 import { KeepLast } from "@web/core/utils/concurrency";
 import { fuzzyLookup } from "@web/core/utils/search";
+import { Pager } from "@web/core/pager/pager";
 
 const { Component, onWillStart, useState } = owl;
 
@@ -10,13 +11,16 @@ export class CustomerList extends Component {
     setup() {
         this.orm = useService("orm");
         this.partners = useState({ data: [] });
+        this.pager = useState({ offset: 0, limit: 20 });
         this.keepLast = new KeepLast();
         this.state = useState({
             searchString: "",
             displayActiveCustomers: false,
         });
         onWillStart(async () => {
-            this.partners.data = await this.loadCustomers();
+            const { length, records } = await this.loadCustomers();
+            this.partners.data = records;
+            this.pager.total = length;
         });
     }
 
@@ -26,12 +30,19 @@ export class CustomerList extends Component {
 
     async onChangeActiveCustomers(ev) {
         this.state.displayActiveCustomers = ev.target.checked;
-        this.partners.data = await this.keepLast.add(this.loadCustomers());
+        this.pager.offset = 0;
+        const { length, records } = await this.keepLast.add(this.loadCustomers());
+        this.partners.data = records;
+        this.pager.total = length;
     }
 
     loadCustomers() {
+        const { limit, offset } = this.pager;
         const domain = this.state.displayActiveCustomers ? [["has_active_order", "=", true]] : [];
-        return this.orm.searchRead("res.partner", domain, ["display_name"]);
+        return this.orm.webSearchRead("res.partner", domain, ["display_name"], {
+            limit: limit,
+            offset: offset,
+        });
     }
 
     filterCustomers(name) {
@@ -41,8 +52,16 @@ export class CustomerList extends Component {
             return this.partners.data;
         }
     }
+
+    async onUpdatePager(newState) {
+        Object.assign(this.pager, newState);
+        const { records } = await this.loadCustomers();
+        this.partners.data = records;
+        this.filterCustomers(this.filterName);
+    }
 }
 
+CustomerList.components = { Pager };
 CustomerList.template = "awesome_tshirt.CustomerList";
 
 CustomerList.props = {
