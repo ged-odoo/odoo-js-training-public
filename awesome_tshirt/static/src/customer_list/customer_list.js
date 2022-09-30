8200dc19a280be1f7ff13f01a851baced5383cf7
Dardenne Florent (dafl) <dafl@odoo.com>
2022-09-30 12:24:12 +0200

    [Solution] 5.7 Refactor the code to use t-model

--- awesome_tshirt/static/src/customer_list/customer_list.js
+++ awesome_tshirt/static/src/customer_list/customer_list.js
@@ -10,41 +10,35 @@ export class CustomerList extends Component {
     setup() {
         this.orm = useService("orm");
         this.partners = useState({ data: [] });
-        this.displayedPartners = useState({ data: [] });
-        this.domain = [];
         this.keepLast = new KeepLast();
-        this.filterName = "";
+        this.state = useState({
+            searchString: "",
+            displayActiveCustomers: false,
+        });
         onWillStart(async () => {
             this.partners.data = await this.loadCustomers();
-            this.displayedPartners.data = this.partners.data;
         });
     }
 
+    get displayedPartners() {
+        return this.filterCustomers(this.state.searchString);
+    }
+
     async onChangeActiveCustomers(ev) {
-        const checked = ev.target.checked;
-        this.domain = checked ? [["has_active_order", "=", true]] : [];
+        this.state.displayActiveCustomers = ev.target.checked;
         this.partners.data = await this.keepLast.add(this.loadCustomers());
-        this.filterCustomers(this.filterName);
     }
 
     loadCustomers() {
-        return this.orm.searchRead("res.partner", this.domain, ["display_name"]);
-    }
-
-    onCustomerFilter(ev) {
-        this.filterName = ev.target.value;
-        this.filterCustomers(ev.target.value);
+        const domain = this.state.displayActiveCustomers ? [["has_active_order", "=", true]] : [];
+        return this.orm.searchRead("res.partner", domain, ["display_name"]);
     }
 
     filterCustomers(name) {
         if (name) {
-            this.displayedPartners.data = fuzzyLookup(
-                name,
-                this.partners.data,
-                (partner) => partner.display_name
-            );
+            return fuzzyLookup(name, this.partners.data, (partner) => partner.display_name);
         } else {
-            this.displayedPartners.data = this.partners.data;
+            return this.partners.data;
         }
     }
 }
